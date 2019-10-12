95c6ebe9964f3cb270652ed761614d204e2e7847
Noxetern <xerogui3@gmail.com>
2019-10-13 00:19:30 +0200

    Create new Xcode project

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Ben Algoet on 13/10/2019.
+//  Copyright © 2019 2019_DEV_204. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
