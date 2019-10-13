b8bb0774c63deea9363bb9081c4394a2130949bf
Noxetern <xerogui3@gmail.com>
2019-10-13 09:06:30 +0200

    Add player enum + unit tests

---
+++ TicTacToeTests/PlayerTests.swift
@@ -0,0 +1,42 @@
+//
+//  PlayerTests.swift
+//  TicTacToeTests
+//
+//  Created by Ben Algoet on 13/10/2019.
+//  Copyright © 2019 2019_DEV_204. All rights reserved.
+//
+
+import XCTest
+@testable import TicTacToe
+
+class PlayerTests: XCTestCase {
+
+    var currentPlayer: Player!
+
+    override func setUp() {
+        super.setUp()
+        currentPlayer = Player.X
+
+    }
+
+    override func tearDown() {
+        currentPlayer = nil
+        super.tearDown()
+    }
+
+    // MARK: - Player tests
+    
+    func testCurrentPlayerX() {
+        XCTAssertEqual(currentPlayer, Player.X)
+    }
+    
+    func testPlayerName() {
+        XCTAssertEqual(currentPlayer.name, "Cross")
+    }
+    
+    func testNextPlayer() {
+        XCTAssertEqual(currentPlayer.next, Player.O)
+        XCTAssertEqual(currentPlayer.next.name, "Nought")
+    }
+
+}
